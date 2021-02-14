858aa235c274f9fb12a05ae1830d09bf6bcdd082
Tobi Ajibade <herityjohnny14>
2021-02-14 01:25:01 -0800

    app: init

---
+++ app.js
@@ -0,0 +1,9 @@
+// load enviroment variables
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log('Server running on port ' + PORT));
